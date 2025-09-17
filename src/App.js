ff35bceacc11f6b2526c38f9a614f09ca96a3b27
Raymond <raymondkibiwott14>
2025-09-18 00:01:24 +0300

    Fix: corrected AppTwo.css import and styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './AppTwo.css';
+import './App.css';
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
